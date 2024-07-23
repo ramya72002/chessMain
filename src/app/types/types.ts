67ef264c7ae1409170be5d7c2590e25b5a67340e
ramya72002 <nsriramya7@gmail.com>
2024-07-23 15:47:14 +0530

    sign in added

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -6,4 +6,27 @@ export interface Session {
     coach_name: string;
     session_link: string;
   }
+  export interface RoundsTiming {
+    start: string;
+    arrivalTime: string;
+    latePlayerNotice: string;
+    under600End: string;
+    under1200End: string;
+  }
+  
+  export interface Section {
+    name: string;
+    registrationFee: string;
+  }
+  
+  export interface Tournament {
+    type: string;
+    name: string;
+    location: string;
+    timeControl: string;
+    upcomingDates: string[];
+    roundsTiming: RoundsTiming;
+    sections: Section[];
+  }
+  
   
