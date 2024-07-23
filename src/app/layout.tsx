89a024917a1a04f5bc795f2c6c4c8de2d04270be
ramya72002 <nsriramya7@gmail.com>
2024-07-23 09:59:48 +0530

    layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import Sidebar from "./sidebar"; // Assuming sidebar is in the same directory
 import PortalHeader from "./portalheader";
 import "./globals.css";
+import Hero from "./hero/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +24,8 @@ export default function RootLayout({
             ||pathname=="/levels/level3"||pathname=="/levels/level4"||pathname=="/chessPuzzle"
           ) && <Sidebar />}
           <div className="content-container">
-            <PortalHeader />
-            {children}
+          {pathname !== "/" && <PortalHeader />}
+          {pathname === "/" ? <Hero /> : children}
           </div>
         </div>
       </body>
