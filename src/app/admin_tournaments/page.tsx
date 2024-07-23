65c368d91aefae8307efe20b72a212a60708b52f
ramya72002 <nsriramya7@gmail.com>
2024-07-23 15:48:54 +0530

    sign in added

--- src/app/admin_tournaments/page.tsx
+++ src/app/admin_tournaments/page.tsx
@@ -11,7 +11,7 @@ const AdminTournaments: React.FC = () => {
   useEffect(() => {
     const fetchTournaments = async () => {
       try {
-        const response = await axios.get<Tournament[]>('http://127.0.0.1:80/tornaments');
+        const response = await axios.get<Tournament[]>('https://backend-chess-tau.vercel.app/tornaments');
         setTournaments(response.data);
       } catch (error) {
         console.error('Error fetching tournaments:', error);
