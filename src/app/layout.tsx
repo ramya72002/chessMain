94be0bbd902d347e11f5323708d55394997d705d
ramya72002 <nsriramya7@gmail.com>
2024-08-08 15:54:18 +0530

    title added1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <div className="layout-container">
           {(pathname === "/portalhome" || 
           pathname === "/imagepuzzle"||pathname === "/puzzleArena"|| pathname === "/learnclass"|| pathname === "/startArena"||pathname=="/puzzles"||pathname=="/levels/level1"||pathname=="/levels/level2"
-            ||pathname=="/levels/level3"||pathname=="/levels/level4"||pathname=="/chessPuzzle"||pathname=="/levels/level2test"||pathname=="/levels/level3test"
+            ||pathname=="/levels/level3"||pathname=="/levels/level4"||pathname=="/chessPuzzle"||pathname=="/ChessPuzzle"||pathname=="/levels/level2test"||pathname=="/levels/level3test"
             ||pathname=="/levels/level4test"||pathname=="/tournaments"||pathname=="/tournamentRegistration"||pathname=="/insidepuzzlearena") && <Sidebar />}
            <div className="content-container">
             {pathname !== "/" && pathname !== "/signin"&&pathname !== "/signup"&& pathname !== "/admin" && pathname !== "/admin_image_puzzles" && pathname !== "/admin_tournaments" && pathname !== "/StudentDetails" && pathname !== "/homeTournament"&& pathname !== "/homeTornRegister"  && <PortalHeader />}
