1036d0d951d92d1845efbf2b5278c687f997f031
ramya72002 <nsriramya7@gmail.com>
2024-08-10 16:03:08 +0530

    delete api added

--- src/app/admin/admin_image_puzzles/page.tsx
+++ src/app/admin/admin_image_puzzles/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './admin_image_puzzles.scss';
 
 interface ImageSet {
+  level: string;
   title: string;
   file_ids: string[];
 }
@@ -11,47 +12,45 @@ interface ImageSet {
 const AdminImagePuzzles: React.FC = () => {
   const [imageSets, setImageSets] = useState<ImageSet[]>([]);
   const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+  const [selectedLevel, setSelectedLevel] = useState<string>('');
   const [selectedTitle, setSelectedTitle] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [imageUrls, setImageUrls] = useState<{ [key: string]: string }>({});
 
-  useEffect(() => {
-    fetchImageSets();
-  }, []);
-
-  const fetchImageSets = () => {
-    setLoading(true);
-    axios.get('https://backend-chess-tau.vercel.app/get')
-      .then(response => {
-        setImageSets(response.data.image_sets);
-        setLoading(false);
-        fetchAllImages(response.data.image_sets);
-      })
-      .catch(error => {
-        setErrorMessage('Error fetching images.');
-        setLoading(false);
-        console.error('Error fetching images:', error);
-      });
+  const fetchImageSets = async (level: string) => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`http://127.0.0.1:80/get_level?level=${level}`);
+      console.log('Fetched image sets:', response.data.image_sets); // Log fetched image sets
+      setImageSets(response.data.image_sets);
+      setLoading(false);
+      fetchAllImages(response.data.image_sets);
+    } catch (error) {
+      setErrorMessage('Error fetching images.');
+      setLoading(false);
+      console.error('Error fetching images:', error);
+    }
   };
 
   const fetchAllImages = (imageSets: ImageSet[]) => {
     imageSets.forEach(set => {
-      set.file_ids.forEach(fileId => {
-        fetchImageFile(fileId);
-      });
+      if (set.file_ids) {
+        set.file_ids.forEach(fileId => {
+          fetchImageFile(fileId);
+        });
+      }
     });
   };
 
-  const fetchImageFile = (fileId: string) => {
-    axios.post('https://backend-chess-tau.vercel.app/image_get_fileid', { file_id: fileId }, { responseType: 'blob' })
-      .then(response => {
-        const url = URL.createObjectURL(new Blob([response.data], { type: response.headers['content-type'] }));
-        setImageUrls(prevState => ({ ...prevState, [fileId]: url }));
-      })
-      .catch(error => {
-        console.error(`Error fetching image with file ID ${fileId}:`, error);
-      });
+  const fetchImageFile = async (fileId: string) => {
+    try {
+      const response = await axios.post('http://127.0.0.1:80/image_get_fileid', { file_id: fileId }, { responseType: 'blob' });
+      const url = URL.createObjectURL(new Blob([response.data], { type: response.headers['content-type'] }));
+      setImageUrls(prevState => ({ ...prevState, [fileId]: url }));
+    } catch (error) {
+      console.error(`Error fetching image with file ID ${fileId}:`, error);
+    }
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -62,48 +61,76 @@ const AdminImagePuzzles: React.FC = () => {
     setSelectedTitle(e.target.value);
   };
 
-  const handleUpload = () => {
-    if (selectedFiles && selectedTitle) {
+  const handleLevelChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const level = e.target.value;
+    console.log('Selected level:', level); // Log the selected level
+    setSelectedLevel(level);
+  };
+
+  const handleFetchImages = () => {
+    if (selectedLevel) {
+      fetchImageSets(selectedLevel);
+    } else {
+      setErrorMessage('Please select a level.');
+    }
+  };
+
+  const handleUpload = async () => {
+    if (selectedFiles && selectedTitle && selectedLevel) {
       const formData = new FormData();
+      formData.append('level', selectedLevel);
       formData.append('title', selectedTitle);
       Array.from(selectedFiles).forEach(file => {
         formData.append('images', file);
       });
 
-      axios.post('https://backend-chess-tau.vercel.app/upload', formData)
-        .then(response => {
-          console.log(response.data.message);
-          fetchImageSets();
-        })
-        .catch(error => {
-          setErrorMessage('Error uploading images.');
-          console.error('Error uploading images:', error);
-        });
+      try {
+        const response = await axios.post('http://127.0.0.1:80/upload', formData);
+        console.log(response.data.message);
+        fetchImageSets(selectedLevel);
+      } catch (error) {
+        setErrorMessage('Error uploading images.');
+        console.error('Error uploading images:', error);
+      }
     } else {
-      setErrorMessage('Please select files and enter a title.');
+      setErrorMessage('Please select files, level, and enter a title.');
     }
   };
 
-  const handleDelete = (title: string) => {
-    if (window.confirm(`Are you sure you want to delete the image set titled "${title}"?`)) {
-      axios.delete('https://backend-chess-tau.vercel.app/delete-arena-title', {
-        data: { title }
-      })
-        .then(response => {
-          console.log(response.data.message);
-          fetchImageSets();
-        })
-        .catch(error => {
-          setErrorMessage(`Error deleting image set titled "${title}".`);
-          console.error('Error deleting image set:', error);
+  const handleDelete = async (title: string, level: string) => {
+    console.log('Deleting:', { title, level }); // Log values
+    if (!title || !level) {
+      setErrorMessage('Title and level must be defined.');
+      return;
+    }
+  
+    if (window.confirm(`Are you sure you want to delete the image set with level "${level}" and title "${title}"?`)) {
+      try {
+        const response = await axios.delete('http://127.0.0.1:80/delete-arena-title', {
+          data: { title, level }
         });
+        console.log(response.data.message);
+        fetchImageSets(selectedLevel);
+      } catch (error) {
+        const errorMessage = error.response?.data?.message || 'Error deleting image set.';
+        setErrorMessage(`Error deleting image set with level "${level}" and title "${title}": ${errorMessage}`);
+        console.error('Error deleting image set:', error);
+      }
     }
   };
+  
 
   return (
     <div className="image-puzzle">
       <h1>Image Puzzle</h1>
       <div className="upload-section">
+        <select value={selectedLevel} onChange={handleLevelChange}>
+          <option value="">Select Level</option>
+          {Array.from({ length: 5 }, (_, i) => (
+            <option key={i} value={`level${i + 1}`}>{`level${i + 1}`}</option>
+          ))}
+        </select>
+        <button onClick={handleFetchImages}>Fetch Images</button>
         <input
           type="text"
           value={selectedTitle}
@@ -120,9 +147,9 @@ const AdminImagePuzzles: React.FC = () => {
       ) : (
         <div className="image-sets">
           {imageSets.map(set => (
-            <div key={set.title} className="image-set">
-              <h2>{set.title}</h2>
-              <button onClick={() => handleDelete(set.title)} className="delete-button">Delete</button>
+            <div key={`${set.title}-${set.level}`} className="image-set">
+               <h2>{set.title} - {set.level || 'No Level'}</h2>
+              <button onClick={() => handleDelete(set.title, set.level || '')} className="delete-button">Delete</button>
               <div className="images-grid">
                 {set.file_ids.map(fileId => (
                   <div key={fileId} className="image-container">
