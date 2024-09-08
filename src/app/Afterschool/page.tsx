8ef4a223f3e5393848b3229f83a8c62f10b4206c
TanujKumar41 <tanujkumar191001@gmail.com>
2024-09-08 18:29:42 +0530

    afterclass

--- src/app/Afterschool/page.tsx
+++ src/app/Afterschool/page.tsx
@@ -1,19 +1,40 @@
-"use client";
+'use client';
 import React from 'react';
-import './Afterschool.scss';
+import Image from 'next/image'; // Assuming you are using Next.js
 import { useRouter } from 'next/navigation';
 import withAuth from '../withAuth';
+import './Afterschool.scss';
 
-// Define the course paths
-const coursePaths: { [key: string]: string } = {
+interface CoursePaths {
+  [key: string]: string;
+}
+
+const coursePaths: CoursePaths = {
   'Basic Checkmates - 2': '/basic-checkmates-2/modules/m1',
   'Basics of Chess': '/modules/m1?afterschool=true',
   'Good Bishop Bad Bishop': '/good-bishop-bad-bishop/modules/m1',
-  'Basic Checkmates': '/basic-checkmates/modules/m1'
+  'Basic Checkmates': '/basic-checkmates/modules/m1',
+  'Basic Checkmates - 3': '/basic-checkmates-2/modules/m1',
+  'Basics of Chess 1': '/modules/m1?afterschool=true',
+  'Basic Checkmates - 4': '/basic-checkmates-2/modules/m1',
+  'Basics of Chess 2': '/modules/m1?afterschool=true',
+  'Good Bishop Bad Bishop 1': '/good-bishop-bad-bishop/modules/m1'
 };
 
-// Define the course card styles
-const courseStyles: { [key: string]: string } = {
+// Define unique images for each course
+const courseImages: CoursePaths = {
+  'Basic Checkmates - 2': '/images/chess-openings.png',
+  'Basics of Chess': '/images/Tactics.png',
+  'Good Bishop Bad Bishop': '/images/Tactics2.png',
+  'Basic Checkmates': '/images/Tactics3.png',
+  'Basic Checkmates - 3': '/images/strategy.png',
+  'Basics of Chess 1': '/images/checkmates.png',
+  'Basic Checkmates - 4': '/images/checkmate2.png',
+  'Basics of Chess 2': '/images/game.png',
+  'Good Bishop Bad Bishop 1': '/images/chesss.png'
+};
+
+const courseStyles: CoursePaths = {
   'Basic Checkmates - 2': 'recently-used',
   'Basics of Chess': 'shapes',
   'Good Bishop Bad Bishop': 'graphics',
@@ -38,20 +59,23 @@ const MyAccount = () => {
         <h1>After School Program</h1>
       </header>
       <div className="courses-info1">
-          <h3>All Courses with Paths</h3>
-        </div>
+        <h3>All Courses with Paths</h3>
+      </div>
 
       <section className="courses-section">
-        
-
         {Object.entries(coursePaths).map(([course, path], index) => (
           <div key={index} className={`course-card ${courseStyles[course]}`}>
-            <div className="course-info">
-              <div className="course-status">
-                <h4>{course}</h4>
+            <div className="course-image-container">
+              <Image
+                src={courseImages[course]} // Use unique images for each course
+                alt={course}
+                layout="fill"
+                objectFit="contain"
+                className="course-image"
+              />
+              <div className="image-overlay">
                 <button
-                  className="progress"
-                  style={{ backgroundColor: 'red' }}
+                  className="in-progress-button"
                   onClick={() => handleViewProgress(course)}
                 >
                   In Progress
